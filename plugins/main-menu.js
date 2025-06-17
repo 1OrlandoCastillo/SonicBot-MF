661734ab4d149e63baa9b8726378a1737b46647a
1OrlandoCastillo <orlandocastillomaria26@gmail.com>
2025-06-17 12:52:42 -0600

    Actualizar main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -163,30 +163,9 @@ const handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
       (_, name) => String(replace[name])
     )
 
-    await conn.sendMessage(
-  m.chat,
-  {
-    image: fs.readFileSync('./storage/img/menu.jpg'),
-    caption: text.trim(),
-    contextInfo: {
-      mentionedJid: conn.parseMention(text.trim()),
-      isForwarded: true,
-      forwardedNewsletterMessageInfo: {
-        newsletterJid: '120363403143798163@newsletter',
-        newsletterName: 'LOVELLOUD',
-        },
-      externalAdReply: {
-        title: 'Anya Forger',
-        body: '',
-        thumbnail: fs.readFileSync('./storage/img/menu2.jpg'),
-        sourceUrl: 'https://dash.lovelloud.uk',
-        mediaType: 1,
-        renderLargerThumbnail: true
-      }
-    }
-  },
-  { quoted: m }
-);
+    const img = `https://files.catbox.moe/hjl3b6.jpg`;
+
+    await conn.sendFile(m.chat, img, 'thumbnail.jpg', text.trim(), m, null, rcanal);
 
   } catch (e) {
     conn.reply(m.chat, '❎ Lo sentimos, el menú tiene un error.', m)
