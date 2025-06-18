806a6fc9180f0b087ce3dddd9d153554d95dde0b
1OrlandoCastillo <orlandocastillomaria26@gmail.com>
2025-06-18 09:13:54 -0600

    Actualizar main-staff.js

--- plugins/main-staff.js
+++ plugins/main-staff.js
@@ -1,73 +1,67 @@
 let handler = async (m, { conn, command, usedPrefix }) => {
-let staff = `🚩 *EQUIPO DE AYUDANTES*
-🤖 *Bot:* ${global.namebot}
+  let staff = `
+🚩 *EQUIPO DE AYUDANTES* 🤖
+*Bot:* ${global.namebot}
 🪐 *Versión:* ${global.vs}
 
-•
-🎩 *Propietario del bot:* 
-💛 *Número:* Wa.me/2731590195
+• 🎩 *Propietario del bot:* 
+  💛 *Número:* Wa.me/2731590195
 
-• 
-🍭 *Rol* Developer
-💛 *Numero:* +505 5786 5603
+• 🍭 *Rol:* Developer 
+  💛 *Numero:* +505 5786 5603
 
-• 
-🎩 *Rol:* Developer
-💛 *Número:*
+• 🎩 *Rol:* Developer 
+  💛 *Número:* 
 
-• 
-🍭 *Rol:* Mod
-💛 *Número:* 
+• 🍭 *Rol:* Mod 
+  💛 *Número:* 
 
+• 🍭 *Rol:* Mod 
+  💛 *Numero:* 
 
-• 
-🍭 *Rol:* Mod
-💛 *Numero:*
+• 🎩 *Rol:* Mod 
+  💛 *Número:* 
 
-• 
-🎩 *Rol:* Mod
-💛 *Número:*
+• 🍭 *Rol:* Developer 
+  💛 *Número:* 
 
-• 
-🍭 *Rol:*  Developer
-💛 *Número:*
+• 🎩 *Rol:* Mod 
+  💛 *Número:* 
 
-•
-🎩 *Rol:* Mod
-💛 *Número:*
+• 🍭 *Rol:* Mod 
+  💛 *Numero:*
+`;
 
-• 
-🍭 *Rol:* Mod
-💛 *Numero:*`
-await conn.sendFile(
-  m.chat,
-  'https://raw.githubusercontent.com/WillZek/Storage-CB/main/images/21396e078a24.jpg',
-  'brook.jpg',
-  staff.trim(),
-  true,
-  {
-    contextInfo: {
-      forwardingScore: 200,
-      isForwarded: false,
-      externalAdReply: {
-        showAdAttribution: true,
-        renderLargerThumbnail: false,
-        title: '🎩 STAFF OFICIAL 🌟',
-        body: namebot,
-        mediaType: 1,
+  await conn.sendFile(
+    m.chat,
+    'https:                                                                             
+    '//raw.githubusercontent.com/WillZek/Storage-CB/main/images/21396e078a24.jpg',
+    'brook.jpg',
+    staff.trim(),
+    true,
+    {
+      contextInfo: {
+        forwardingScore: 200,
+        isForwarded: false,
+        externalAdReply: {
+          showAdAttribution: true,
+          renderLargerThumbnail: false,
+          title: '🎩 STAFF OFICIAL 🌟',
+          body: global.namebot,
+          mediaType: 1,
+        },
       },
     },
-  },
-  {
-    mentions: m.sender,
-  }
-);
+    {
+      mentions: m.sender,
+    }
+  );
 
-m.react(💎);
+  m.react('💎');
+};
 
-}
-handler.help = ['staff']
-handler.command = ['colaboradores', 'staff']
-handler.tags = ['main', 'crow']
+handler.help = ['staff'];
+handler.command = ['colaboradores', 'staff'];
+handler.tags = ['main', 'crow'];
 
-export default handler
+export default handler;
