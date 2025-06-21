d29cfaa6dd35782db995948e684c4f83eae8b5a0
1OrlandoCastillo <orlandocastillomaria26@gmail.com>
2025-06-21 13:11:54 -0600

    Actualizar Minovio.js

--- plugins/Minovio.js
+++ plugins/Minovio.js
@@ -4,7 +4,7 @@ import fetch from 'node-fetch';
 let handler = async (m, { conn }) => {
   let user = global.db.data.users[m.sender];
   let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender;
-  let pp = await conn.profilePictureUrl(who, 'image').catch(_ => './storage/img/menu.jpg');
+  let pp = await conn.profilePictureUrl(who, 'image').catch(_ => 'https://qu.ax/qhKfn.jpg');
   let name = conn.getName(who);
 
   let str = `𝙀𝙎𝙏𝙀 𝙀𝙎 𝙈𝙄 𝙉𝙊𝙑𝙄𝙊, *¿* 𝙀𝙎 𝙃𝙀𝙍𝙈𝙊𝙎𝙊 𝙑𝙀𝙍𝘿𝘼𝘿 *?* 😍 @${who.replace(/@.+/, '')} 𝙀𝙍𝙀𝙎 𝙀𝙇 𝙈𝙀𝙅𝙊𝙍 𝙉𝙊𝙑𝙄𝙊 𝘿𝙀𝙇 𝙈𝙐𝙉𝘿𝙊.🫶🏻♥️ `.trim();
