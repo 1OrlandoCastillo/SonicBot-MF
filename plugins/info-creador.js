4aeb6ddac76f5f98a6b081a78f13785f1da32915
Orlando <orlandocastillomaria26@gmail.com>
2025-07-01 15:51:50 -0600

    Actualizar info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn }) => {
-  let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;White444;;\nFN:White444\nWhite444\nTEL;waid=5212731590195:5212731590195\nEND:VCARD`
+  let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;White444;;\nFN:White444\nTEL;waid=5212731590195:5212731590195\nEND:VCARD`
   await conn.sendMessage(m.chat, { contacts: { displayName: 'White444', contacts: [{ vcard }] }}, { quoted: m })
 }
 
