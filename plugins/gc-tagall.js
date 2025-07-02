ebeca50f5173adb25e810be9cf981cf832903776
Orlando <orlandocastillomaria26@gmail.com>
2025-07-02 16:28:33 -0600

    Actualizar gc-tagall.js

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -1,10 +1,14 @@
-const handler = async (m, { conn, args, participants }) => {
-  const message = args.join(' ');
+const handler = async (m, { conn, args, participants, isAdmin }) => {
+  if (!isAdmin) {
+    return conn.sendMessage(m.chat, { text: '*Solo los administradores pueden usar este comando.*' }, { quoted: m });
+  }
+
+  const message = args.join(' ') || '';
   const mentionText = `*! MENCION GENERAL !*\n*PARA ${participants.length} MIEMBROS* 🗣️\n\n*Mensaje:* ${message}\n\n`;
   const mentions = participants.map((a) => a.id);
   const text = `${mentionText}${mentions.map((id) => `@${id.split('@')[0]}`).join('\n')}`;
 
-  conn.sendMessage(m.chat, { text, mentions });
+  return conn.sendMessage(m.chat, { text, mentions });
 };
 
 handler.help = ['todos *<mensaje opcional>*'];
