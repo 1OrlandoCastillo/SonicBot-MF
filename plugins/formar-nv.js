19027c4108a93b87f753a4e384b94224ba458717
Orlando <orlandocastillomaria26@gmail.com>
2025-07-02 20:31:31 -0600

    Actualizar formar-nv.js

--- plugins/formar-nv.js
+++ plugins/formar-nv.js
@@ -4,13 +4,18 @@ function handler(m, { groupMetadata }) {
   // Obtener los participantes del grupo
   let ps = groupMetadata.participants.map(v => v.id);
 
+  // Si hay menos de 2 participantes, avisa y termina
+  if (ps.length < 2) {
+    return m.reply('El grupo no tiene suficientes participantes para formar una pareja.');
+  }
+
   // Seleccionar un usuario al azar
-  let a = ps.getRandom();
+  let a = ps[Math.floor(Math.random() * ps.length)];
   let b;
 
   // Asegurarse de que no se seleccione el mismo usuario
   do {
-    b = ps.getRandom();
+    b = ps[Math.floor(Math.random() * ps.length)];
   } while (b === a);
 
   // Enviar el mensaje de la pareja seleccionada
