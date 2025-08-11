cc790b72f83df1d626a0fa9d69720c8569f18987
Orlando <orlandocastillomaria26@gmail.com>
2025-08-11 17:40:20 -0600

    Actualizar fun-formartrio.js

--- plugins/fun-formartrio.js
+++ plugins/fun-formartrio.js
@@ -1,18 +1,38 @@
 let toM = a => '@' + a.split('@')[0]
-function handler(m, { groupMetadata }) {
-let ps = groupMetadata.participants.map(v => v.id)
-let a = ps.getRandom()
-let b
-do b = ps.getRandom()
-while (b === a)
-let c
-do c = ps.getRandom()
-while (b === a)
-m.reply(`*Hey!!! ${toM(a)}, ${toM(b)} y ${toM(c)} han pensado en hacer un trio? ustedes 3 hacen un buen trio 😳😏*`, null, {
-mentions: [a, b, c],
-})}
+
+function getRandomFromArray(arr) {
+  return arr[Math.floor(Math.random() * arr.length)]
+}
+
+async function handler(m, { conn, groupMetadata }) {
+  let ps = groupMetadata.participants.map(v => v.id)
+
+  const steps = 10
+  for (let i = 1; i <= steps; i++) {
+    let percent = i * 10
+    let progressBar = '■'.repeat(i) + '□'.repeat(steps - i)
+    await conn.reply(m.chat, `⌛ Buscando persona... [${progressBar}] ${percent}%`, m)
+    await new Promise(r => setTimeout(r, 400))
+  }
+
+  let a = getRandomFromArray(ps)
+  let b
+  do {
+    b = getRandomFromArray(ps)
+  } while (b === a)
+
+  let c
+  do {
+    c = getRandomFromArray(ps)
+  } while (c === a || c === b)
+
+  m.reply(`*Hey!!! ${toM(a)}, ${toM(b)} y ${toM(c)} han pensado en hacer un trio? ustedes 3 hacen un buen trio 😳😏*`, null, {
+    mentions: [a, b, c],
+  })
+}
+
 handler.help = ['formartrio']
 handler.tags = ['fun']
-handler.command = ['formartrio','formartrios']
+handler.command = ['formartrio', 'formartrios']
 handler.group = true
 export default handler
