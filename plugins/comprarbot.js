dae5296008727e72a7432a2b3c687e39c5a014f3
Orlando <orlandocastillomaria26@gmail.com>
2025-08-14 12:24:03 -0600

    Actualizar comprarbot.js

--- plugins/comprarbot.js
+++ plugins/comprarbot.js
@@ -2,15 +2,21 @@ const handler = async (m, { conn }) => {
   m.reply(global.ComprarBot);
 };
 
-handler.command = /^(comprarbot|comprar)$/i; // Regex que detecta ambos comandos
+handler.command = /^(comprarbot|comprar)$/i;
 export default handler;
 
 global.ComprarBot = `
-〔 *TIPOS DE COMPRA* 〕
+✦✧✦✧✦✧✦✧✨🔥 *VENTA DE BOTS* 🔥✨✦✧✦✧✦✧✦✧
 
-*BOT PARA GRUPO* :
-> wa.me/+522731590195
+💥 🚀 *Automatiza tu grupo y sorprende a todos* 🚀 💥
+💫 ✨ *Soporte 24/7 + Actualizaciones GRATIS* ✨ 💫
 
-*BOT PARA GRUPO PERMANENTE* :
-> wa.me/+522731590195
+⚡💎 *BOT PARA GRUPO* 💎⚡
+   📲 wa.me/+522731590195
+⚡♾️ *BOT PARA GRUPO PERMANENTE* ♾️⚡
+   📲 wa.me/+522731590195
+
+✨💫⚡ _Rápido • Seguro • Personalizado • Eficiente_ ⚡💫✨
+
+✦✧✦✧✦✧✦✧✨🔥 *NO PIERDAS ESTA OPORTUNIDAD* 🔥✨✦✧✦✧✦✧✦✧
 `;
