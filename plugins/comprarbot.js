09728d8e647e1126977840cc27d429d5edb7acd8
Orlando <orlandocastillomaria26@gmail.com>
2025-08-14 12:19:51 -0600

    Actualizar comprarbot.js

--- plugins/comprarbot.js
+++ plugins/comprarbot.js
@@ -1,7 +1,8 @@
-const handler = async (m, {conn}) => {
+const handler = async (m, { conn }) => {
   m.reply(global.ComprarBot);
 };
-handler.command ='comprarbot',/^(ComprarBot|Comprar|comprar|ComprarBot)$/i;
+
+handler.command = /^(comprarbot|comprar)$/i; // Regex que detecta ambos comandos
 export default handler;
 
 global.ComprarBot = `
@@ -10,6 +11,6 @@ global.ComprarBot = `
 *BOT PARA GRUPO* :
 > wa.me/+522731590195
 
-*BOT PARA  GRUPO PERMANENTE* :
+*BOT PARA GRUPO PERMANENTE* :
 > wa.me/+522731590195
 `;
