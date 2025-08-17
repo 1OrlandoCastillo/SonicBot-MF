90bb6741ae8c7cd7d1b6314b9f528bb527440827
Orlando <orlandocastillomaria26@gmail.com>
2025-08-17 00:07:39 -0600

    Actualizar info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -11,36 +11,34 @@ let handler = async (m, { conn }) => {
     "▰▰▰▰▰▰▰▰▱▱ 80%",
     "▰▰▰▰▰▰▰▰▰▱ 90%",
     "▰▰▰▰▰▰▰▰▰▰ 100%\n\n✅ Listo!"
-  ]
+  ];
 
-  let msg
-  for (let i = 0; i < loading.length; i++) {
-    if (msg) {
-      await conn.sendMessage(m.chat, { delete: msg.key }) // eliminar el mensaje anterior
-    }
-    msg = await conn.sendMessage(m.chat, { text: loading[i] }, { quoted: m })
-    await new Promise(res => setTimeout(res, 500)) // medio segundo
-  }
+  // Enviamos el mensaje inicial
+  let msg = await conn.sendMessage(m.chat, { text: loading[0] }, { quoted: m });
 
-  // Eliminar la barra final
-  if (msg) {
-    await conn.sendMessage(m.chat, { delete: msg.key })
+  // Vamos editando el mismo mensaje
+  for (let i = 1; i < loading.length; i++) {
+    await new Promise(res => setTimeout(res, 500)); // esperar medio segundo
+    await conn.sendMessage(m.chat, { 
+      text: loading[i],
+      edit: msg.key  // <-- aquí se edita el mensaje original
+    });
   }
 
   // Mensaje previo al contacto
-  await conn.sendMessage(m.chat, { text: "⚡ Hola, soy SonicBot-ProMax ⚡\nAquí está el contacto de mi creador" }, { quoted: m })
+  await conn.sendMessage(m.chat, { text: "⚡ Hola, soy SonicBot-ProMax ⚡\nAquí está el contacto de mi creador" }, { quoted: m });
 
   // VCard del owner
-  let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;White444;;\nFN:White444\nTEL;waid=5212731590195:5212731590195\nEND:VCARD`
+  let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;White444;;\nFN:White444\nTEL;waid=5212731590195:5212731590195\nEND:VCARD`;
   await conn.sendMessage(
     m.chat,
     { contacts: { displayName: 'White444', contacts: [{ vcard }] } },
     { quoted: m }
-  )
+  );
 }
 
-handler.help = ['owner']
-handler.tags = ['main']
-handler.command = ['owner', 'creator', 'creador', 'dueño']
+handler.help = ['owner'];
+handler.tags = ['main'];
+handler.command = ['owner', 'creator', 'creador', 'dueño'];
 
-export default handler
+export default handler;
