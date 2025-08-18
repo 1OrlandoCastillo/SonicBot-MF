03c139713537ca4e457d0adf0753b0b5299ceff8
Orlando <orlandocastillomaria26@gmail.com>
2025-08-17 18:18:24 -0600

    Actualizar fun-top.js

--- plugins/fun-top.js
+++ plugins/fun-top.js
@@ -1,10 +1,10 @@
-let user = a => '@' + a.split('@')[0]
+let user = a => '@' + a.split('@')[0]  // Esto ya limpia todo después de @
 
 function handler(m, { groupMetadata, conn, text }) {
   if (!text) return conn.reply(m.chat, `⚡ Por favor, ingrese un texto para hacer un Top 10 *texto*.`, m)
 
-  // Lista de participantes
-  let ps = groupMetadata.participants.map(v => v.id)
+  // Lista de participantes sin duplicados
+  let ps = [...new Set(groupMetadata.participants.map(v => v.id))]
 
   // Mezclar lista aleatoriamente
   let shuffled = ps.sort(() => 0.5 - Math.random())
@@ -12,14 +12,12 @@ function handler(m, { groupMetadata, conn, text }) {
   // Tomar los primeros 10 (o menos si no hay tantos miembros)
   let top10 = shuffled.slice(0, 10)
 
-  // Elegir un emoji y un sonido aleatorio
-  let k = Math.floor(Math.random() * 70)
+  // Elegir un emoji aleatorio
   let x = pickRandom(['🤓','😅','😂','😳','😎','🥵','😱','🤑','🙄','💩','🍑','🤨','🥴','🔥','👇🏻','😔','👀','🌚'])
-  let vn = `https://hansxd.nasihosting.com/sound/sound${k}.mp3`
 
-  // Construir mensaje
+  // Construir mensaje sin que aparezcan * en los nombres
   let top = `*${x} Top 10 ${text} ${x}*\n\n` +
-    top10.map((u, i) => `*${i + 1}. ${user(u)}*`).join('\n')
+    top10.map((u, i) => `${i + 1}. ${user(u)}`).join('\n')  // <-- sin asteriscos alrededor
 
   m.reply(top, null, { mentions: top10 })
 }
@@ -28,7 +26,7 @@ handler.help = ['top *<texto>*']
 handler.command = ['top']
 handler.tags = ['fun']
 handler.group = true
-handler.register = false // <- ahora NO requiere registro
+handler.register = false
 
 export default handler
 
