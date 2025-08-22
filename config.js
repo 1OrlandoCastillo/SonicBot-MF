844cb986ef2ada18e9151aacf3ba9316966ca892
Orlando <orlandocastillomaria26@gmail.com>
2025-08-21 20:07:32 -0600

    Actualizar config.js

--- config.js
+++ config.js
@@ -1,54 +1,45 @@
-import { watchFile, unwatchFile } from 'fs';
-import chalk from 'chalk';
-import { fileURLToPath } from 'url';
+import { watchFile, unwatchFile } from 'fs' 
+import chalk from 'chalk'
+import { fileURLToPath } from 'url'
 
-// ------------------ Dueño ------------------
-global.ownerNumber = '5212731590195@s.whatsapp.net'; // Tu número completo
+
 global.owner = [
   ['5212731590195', 'White444', true],
-];
+]
 
-// Mods y Premiums
-global.mods = [];
-global.prems = [];
 
-// Información del bot
-global.libreria = 'Baileys';
-global.baileys = 'V 6.7.16';
-global.vs = '2.2.0';
-global.nameqr = 'YukiBot-MD';
-global.namebot = '✿◟Yυƙι-Sυσυ-Bσƚ◞✿';
-global.sessions = 'Sessions';
-global.jadi = 'JadiBots';
-global.yukiJadibts = true;
+global.mods = []
+global.prems = []
 
-// Stickers
-global.packname = 'SonicBot';
-global.namebot = 'SonicBot-MF';
-global.author = 'White444';
+global.libreria = 'Baileys'
+global.baileys = 'V 6.7.16' 
+global.vs = '2.2.0'
+global.nameqr = 'YukiBot-MD'
+global.namebot = '✿◟Yυƙι-Sυσυ-Bσƚ◞✿'
+global.sessions = 'Sessions'
+global.jadi = 'JadiBots' 
+global.yukiJadibts = true
 
-// Canal oficial
-global.namecanal = 'SonicBot-MF Official';
-global.canal = 'https://whatsapp.com/channel/0029Vb3oShrICVfiTWhDHM13';
-global.idcanal = '120363411154070926@newsletter';
+global.packname = 'SonicBot'
+global.namebot = 'SonicBot-MF'
+global.author = 'White444'
+
+
+global.namecanal = 'SonicBot-MF Official'
+global.canal = 'https://whatsapp.com/channel/0029Vb3oShrICVfiTWhDHM13'
+global.idcanal = '120363411154070926@newsletter'
 
 global.ch = {
-  ch1: '120363411154070926@newsletter',
-};
+ch1: '120363411154070926@newsletter',
+}
 
-// Configuración adicional
-global.multiplier = 69;
-global.maxwarn = '2';
+global.multiplier = 69 
+global.maxwarn = '2'
 
-// ------------------ Auto reload seguro ------------------
-let file = fileURLToPath(import.meta.url);
+
+let file = fileURLToPath(import.meta.url)
 watchFile(file, () => {
-  unwatchFile(file);
-  console.log(chalk.greenBright("✔️  Config.js actualizado"));
-  
-  try {
-    import(`${file}?update=${Date.now()}`);
-  } catch (err) {
-    console.log(chalk.redBright("❌ Error al recargar config.js"), err);
-  }
-});
+  unwatchFile(file)
+  console.log(chalk.redBright("Update 'config.js'"))
+  import(`${file}?update=${Date.now()}`)
+})
