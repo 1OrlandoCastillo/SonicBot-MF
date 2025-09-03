556bc61e1f8b5670415c4921d2200cf52f7b2901
Orlando <orlandocastillomaria26@gmail.com>
2025-09-03 13:18:26 -0600

    Actualizar handler.js

--- handler.js
+++ handler.js
@@ -6,9 +6,7 @@ import { unwatchFile, watchFile } from 'fs'
 import chalk from 'chalk'
 import fetch from 'node-fetch'
 
-// Ya no necesitas esto porque smsg lo maneja correctamente
-// const { proto } = await import('@whiskeysockets/baileys')
-
+const { proto } = (await import('@whiskeysockets/baileys')).default
 const isNumber = x => typeof x === 'number' && !isNaN(x)
 const delay = ms => isNumber(ms) && new Promise(resolve => setTimeout(function () {
   clearTimeout(this)
@@ -295,7 +293,8 @@ export async function handler(chatUpdate) {
         premium: `✤ Hola, este comando solo puede ser utilizado por Usuarios *Premium*.`,
         group: `✤ Hola, este comando solo puede ser utilizado en *Grupos*.`,
         private: `✤ Hola, este comando solo puede ser utilizado en mi Chat *Privado*.`,
-        admin: `✤ Hola, la bot debe ser *Administradora* para ejecutar este Comando.`,
+        admin: `✤ Hola, este comando solo puede ser utilizado por los *Administradores* del Grupo.`,
+        botAdmin: `✤ Hola, la bot debe ser *Administradora* para ejecutar este Comando.`,
         unreg: `✤ Hola, para usar este comando debes estar *Registrado.*`,
         restrict: `✤ Hola, esta característica está *deshabilitada.*`
       }[type]
@@ -325,12 +324,12 @@ export async function handler(chatUpdate) {
           if (!isNumber(stat.total)) stat.total = 1
           if (!isNumber(stat.success)) stat.success = m.error != null ? 0 : 1
           if (!isNumber(stat.last)) stat.last = now
-          if (!isNumber(stat.lastSuccess)) stat.lastSuccess = now
+          if (!isNumber(stat.lastSuccess)) stat.lastSuccess = m.error != null ? 0 : now
         } else stat = stats[m.plugin] = {
           total: 1,
           success: m.error != null ? 0 : 1,
           last: now,
-          lastSuccess: now
+          lastSuccess: m.error != null ? 0 : now
         }
         stat.total += 1
         stat.last = now
