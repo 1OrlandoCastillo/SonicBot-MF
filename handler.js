a79f436737256b4a13d9de0e276e56585cdabee8
Orlando <orlandocastillomaria26@gmail.com>
2025-09-05 10:17:28 -0600

    Actualizar handler.js

--- handler.js
+++ handler.js
@@ -118,8 +118,8 @@ const isBotAdmin = bot?.admin || false
 
 const ___dirname = path.join(path.dirname(fileURLToPath(import.meta.url)), './plugins')  
 
-global.idcanal = '120363403143798163@newsletter'  
-global.namecanal = 'LOVELLOUD Official Channel'  
+global.idcanal = '120363411154070926@newsletter'  
+global.namecanal = 'SONICBOT-MF Official Channel'  
 global.rcanal = {  
   contextInfo: {  
     isForwarded: true,  
