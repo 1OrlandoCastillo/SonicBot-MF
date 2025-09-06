968c07ab0dccbf12777f2669d1b412e282b43c06
AdrianDH7 <adriandiazhurtado9@gmail.com>
2025-09-05 22:46:37 -0600

    Update welcome-despedida.js

--- plugins/welcome-despedida.js
+++ plugins/welcome-despedida.js
@@ -1,4 +1,4 @@
-import Jimp from 'jimp'
+import Jimp from "jimp"
 
 const handler = async (update, { conn }) => {
   try {
@@ -6,57 +6,51 @@ const handler = async (update, { conn }) => {
     const action = update.action
     const chatId = update.id
 
-    if (!participants || !['add','remove'].includes(action)) return
+    if (!["add", "remove"].includes(action)) return
 
-    // Asegurar configuración básica
-    if (!global.db.data.settings?.[chatId]) {
-      global.db.data.settings[chatId] = {
-        welcome: true,
-        despedida: true
-      }
-      await global.db.write()
-    }
-
-    const groupSettings = global.db.data.settings[chatId]
-    const groupName = (await conn.groupMetadata(chatId))?.subject || 'este grupo'
+    const groupMetadata = await conn.groupMetadata(chatId)
+    const groupName = groupMetadata.subject || "este grupo"
 
     for (let user of participants) {
       const nombre = await conn.getName(user)
-      let msgText = ''
       let avatar
 
       try {
-        avatar = await conn.profilePictureUrl(user, 'image')
+        avatar = await conn.profilePictureUrl(user, "image")
       } catch {
-        avatar = 'https://telegra.ph/file/24fa902ead26340f3df2c.png'
+        avatar = "https://telegra.ph/file/24fa902ead26340f3df2c.png"
       }
 
-      if (action === 'add' && groupSettings.welcome) {
-        msgText = `👋 Bienvenido *@${user.split('@')[0]}* a *${groupName}*`
-      } else if (action === 'remove' && groupSettings.despedida) {
-        msgText = `👋 *@${user.split('@')[0]}* se fue...\n😈 QUE BUENO QUE SE SALIÓ`
-      } else continue
+      // Texto según acción
+      let msgText = ""
+      if (action === "add") {
+        msgText = `👋 Bienvenido *@${user.split("@")[0]}* a *${groupName}*`
+      } else if (action === "remove") {
+        msgText = `👋 *@${user.split("@")[0]}* se fue...\n😈 QUE BUENO QUE SE SALIÓ`
+      }
 
-      // Imagen personalizada con Jimp
+      // Editar imagen con Jimp
       const image = await Jimp.read(avatar)
       image.cover(512, 512)
       const font = await Jimp.loadFont(Jimp.FONT_SANS_32_WHITE)
-      image.print(font, 10, 470, { text: nombre, alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER })
+      image.print(font, 10, 470, {
+        text: nombre,
+        alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
+      })
 
       const buffer = await image.getBufferAsync(Jimp.MIME_PNG)
 
-      // Enviar mensaje con mención + foto
-      await conn.sendMessage(chatId, { 
-        image: buffer, 
-        caption: msgText, 
-        mentions: [user] 
+      // Enviar imagen + mención
+      await conn.sendMessage(chatId, {
+        image: buffer,
+        caption: msgText,
+        mentions: [user],
       })
     }
-
   } catch (e) {
-    console.error('❌ Error en welcome/despedida:', e)
+    console.error("❌ Error en welcome/despedida:", e)
   }
 }
 
-handler.event = 'group-participants.update'
+handler.event = "group-participants.update"
 export default handler
