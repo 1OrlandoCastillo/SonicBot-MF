d26ff66a0b6f3c5ad19c29b3369d955b2ae8bfb5
AdrianDH7 <adriandiazhurtado9@gmail.com>
2025-09-06 15:18:14 -0600

    Actualizar welcome.js

--- plugins/welcome.js
+++ plugins/welcome.js
@@ -1,21 +1,22 @@
-const handler = async (m, { conn, participants }) => {
-  if (!m.isGroup) return
+const handler = async (update) => {
+  const { conn } = global
+  const { participants, action, id } = update
+
+  if (action !== 'add') return // Solo cuando alguien entra
+  if (!id) return
 
   for (let user of participants) {
-    const pushname = user.notify || user.vname || 'Invitado'
-    const chat = m.chat
+    const pushname = user.notify || 'Invitado'
+    const chat = id
 
     const welcomeText = 
-`👋 ¡Hola @${user.id.split('@')[0]}!
+`👋 ¡Hola @${user.split('@')[0]}!
 Bienvenido al grupo.
 Esperamos que te diviertas y participes activamente. 🥳`
 
-    await conn.sendMessage(chat, { text: welcomeText, mentions: [user.id] })
+    await conn.sendMessage(chat, { text: welcomeText, mentions: [user] })
   }
 }
 
 handler.event = 'group-participants.update'
-handler.group = true
-handler.tags = ['grupo']
-handler.command = ['welcome']
 export default handler
